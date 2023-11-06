7b5016c3219653016a8c9fff261062e40d586c27
Ignaciod1994 <137319183+Ignaciod1994@users.noreply.github.com>
2023-11-06 13:18:15 -0300

    Add files via upload

---
+++ js/miarchivo.js
@@ -0,0 +1,95 @@
+function informacionObligatoria (nombre, apellido, edad, documento, email , cantidad, cuotas, cuotaMensual) { 
+  
+  this.nombre= nombre; 
+  this.apellido= apellido; 
+  this.edad= edad; 
+  this.documento= documento;
+  this.email = email;
+  this.cantidad = cantidad;  
+  this.cuotas = cuotas;
+  this.cuotaMensual = cuotaMensual;
+  
+  }
+
+  const prestamos = JSON.parse(localStorage.getItem("usuarios")) || []
+
+
+  function dividir(dato1, dato2) {
+    let resultado = dato1 / dato2;
+    return resultado;
+  }
+  
+  function calcularCuota(cantidad, tasa, cuotas) {
+    let tasaMensual = tasa / 100 / 12;
+    let cuota = cantidad * (tasaMensual * Math.pow(1 + tasaMensual, cuotas)) / ((Math.pow(1 + tasaMensual, cuotas)) - 1);
+    return cuota;
+  }
+
+ const formulario = document.getElementById ('formulario');
+ formulario.addEventListener('submit', enviarForm);
+
+
+  function enviarForm (e) {
+   e.preventDefault()
+  const nombre = document.getElementById('inputnombre').value;
+  const apellido = document.getElementById('inputapellido').value;
+  const edad =  parseInt (document.getElementById('inputedad').value);
+  const documento = parseInt (document.getElementById('inputdni').value);
+  const email = document.getElementById('inputemail').value;
+  const cantidad = parseInt (document.getElementById('inputcantidad').value);
+  const cuotas = parseInt (document.getElementById('inputcuotas').value);
+  
+
+  const tasaDeInteres = 75;
+    
+
+  const prestamoUsuario = {
+     
+    nombre : nombre,
+    apellido : apellido, 
+    edad : edad,
+    documento : documento, 
+    email : email,
+   
+
+  }
+
+  localStorage.setItem ("usuario", JSON.stringify(prestamoUsuario))
+  
+
+  const resultadoDiv = document.getElementById ('resultado');
+
+  if (cantidad > 10000) {
+    const cuotaPrestamo = calcularCuota(cantidad, tasaDeInteres, cuotas);
+    resultadoDiv.innerHTML= `Usted debe pagar ${cuotas} cuotas de ${cuotaPrestamo.toFixed(2)}`;
+
+    const prestamo = new informacionObligatoria (nombre, apellido, edad, documento, email, cantidad, cuotas, cuotaPrestamo)
+    prestamos.push (prestamo);
+    localStorage.setItem ("usuarios", JSON.stringify(prestamos))
+
+
+  } else {
+    resultadoDiv.innerHTML ="El monto solicitado es menor al mínimo, por favor ingrese un importe mayor a 10000";
+  }
+
+ }
+
+ const encontradoDiv = document.getElementById ('encontrado');
+ 
+ const buscarPrestamoPorNombre = () => { 
+  const nombreBuscado = document.getElementById('buscarprestamo').value;
+  const prestamoEncontrado = prestamos.find((prestamo) => prestamo.nombre.toLowerCase().includes(nombreBuscado.toLowerCase()));
+  encontradoDiv.innerHTML (prestamoEncontrado);
+
+
+  if (prestamoEncontrado) {
+   encontradoDiv.innerHTML = `Préstamo encontrado: Nombre: ${prestamoEncontrado.nombre} Apellido ${prestamoEncontrado.apellido} Cantidad: ${prestamoEncontrado.nombre}, Cuotas: ${prestamoEncontrado.cuotas}, Cuota Mensual: ${prestamoEncontrado.cuotaMensual.toFixed(2)}`
+
+  } else {
+   encontradoDiv.innerHTML = "No se encontró ningún préstamo para el nombre proporcionado"
+  }
+ }
+
+
+const buscarButton = document.getElementById ('buscarButton');
+buscarButton.addEventListener ('click', buscarPrestamoPorNombre);
